29dec042d166127ee7d098b2d89be477bcfc05c2
Kevin Deng <ktd9@duke.edu>
2018-03-29 16:46:45 -0400

    Updated Worldview and level interface

--- doc/api/LevelInterface.java
+++ doc/api/LevelInterface.java
@@ -1,5 +1,18 @@
 
+/**
+ * 
+ * @author Kevin D
+ *
+ */
 
+/**
+ * A Level is a data structure that can be written to and read by the Game Authoring as well as the Game Player.
+ * Through the Level interface, the Game Authoring should be able to add and remove entities in the given Level.
+ * The Game Player should be able to read from these entities from the Level.
+ * The general functionality for the Level module is shown below:
+ * @author Kevin D
+ *
+ */
 public interface LevelInterface{
 	/**
 	 * Returns all entities present in the level
--- doc/api/WorldViewInterface.java
+++ doc/api/WorldViewInterface.java
@@ -1,5 +1,8 @@
-package voogasalad_callussalad;
-
+/**
+ * Controls which level and where in the level the view should display; defines some scrolling and window behavior
+ * @author Kevin D
+ *
+ */
 public interface WorldViewInterface{
 	/**
 	 * Returns the geographical position of the level 
