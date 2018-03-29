b9540abc23b6cfb5e7c8b12d6dc9c77fbeb3d8d0
Dana Park <dana.park@duke.edu>
2018-03-29 16:58:56 -0400

    Update GamePlayerUseCase.java

--- doc/usecases/GamePlayerUseCase.java
+++ doc/usecases/GamePlayerUseCase.java
@@ -1,20 +1,24 @@
-### The user saves game progress and is able to play from their saved point later
+/**
+ * USE CASE TO SOLVE: The user saves game progress and is able to play from their saved point later
+ */
+ 
+ 
+/**
+ * Using the interfaces MenuData, ViewData, ExportData, and ImportData, we will call these methods to save the game progress:
+ * First, the Menu Data and View Data will be updated, and then the current preferences and state will be exported.
+ */
 
+updateMenuData()
+updateViewData()
+exportPreferences()
+exportCurrentState()
 
-Using the interfaces MenuData, ViewData, ExportData, and ImportData, we will call these methods to save the game progress:
-
-1. updateMenuData()
-2. updateViewData()
-3. exportPreferences()
-4. exportCurrentState()
-
-* First, the Menu Data and View Data will be updated, and then the current preferences and state will be exported.
-
-The methods below will be used to allow the player to restart the game from a saved point later on:
-
+/**
+ * The methods below will be used to allow the player to restart the game from a saved point later on:
+ * First, the current preferences and game state will be imported, and then the Menu will be populated with the correct setting and the HUD will be set.
+ */
 1. importPreferences()
 2. importGameState()
 3. populateMenu()
 4. setHUD()
 
-* First, the current preferences and game state will be imported, and then the Menu will be populated with the correct setting and the HUD will be set.
