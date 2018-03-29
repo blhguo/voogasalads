0428102abcddfb6cf64e03ee10890bce7ec01704
Dana Park <dana.park@duke.edu>
2018-03-29 17:05:42 -0400

    Update GamePlayerUseCase.java

--- doc/usecases/GamePlayerUseCase.java
+++ doc/usecases/GamePlayerUseCase.java
@@ -8,11 +8,13 @@
  * First, the Menu Data and View Data will be updated, and then the current preferences and state will be exported.
  */
 
-public void saveData(){
-    updateMenuData()
-    updateViewData()
-    exportPreferences()
-    exportCurrentState()
+public void saveData() {
+    
+    updateMenuData(); //this method will update DataManager from the Menu HBox information on top of the screen
+    updateViewData(); //this method will update DataManager as ViewManager updates
+    exportPreferences(); //this method will export the current player preferences
+    exportCurrentState(); //this method will export the current game state
+    
     }
 
 /**
@@ -21,9 +23,11 @@ public void saveData(){
  */
  
 public void loadData(){
-    importPreferences()
-    importGameState()
-    populateMenu()
-    setHUD()
+    
+    importPreferences(); //this method will import current player preferences
+    importGameState(); //this method will import current game state
+    populateMenu(); //this method will populate the Menu HBox with correct settings during initialization
+    setHUD(); //this method will set the HUD to the correct settings during initialization
+    
     }
 
