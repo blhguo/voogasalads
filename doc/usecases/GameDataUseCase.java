6113164f2c267b152c392b65f9786ed04bfc3d01
Harry Wang <harry.wang@duke.edu>
2018-03-29 17:22:52 -0400

    Update GameDataUseCase.java

--- doc/usecases/GameDataUseCase.java
+++ doc/usecases/GameDataUseCase.java
@@ -4,15 +4,13 @@
  
  
 /**
- * Using the interfaces MenuData, ViewData, ExportData, and ImportData, we will call these methods to save the game progress:
- * First, the Menu Data and View Data will be updated, and then the current preferences and state will be exported.
+ * Using the interfaces DataManipulation for us to manipulate data to be serialized/deserialized and saved. 
  */
 
-public void SaveData() {
+public SaveData implements DataManipulation {
     
-    updateMenuData(); //this method will update DataManager from the Menu HBox information on top of the screen
-    updateViewData(); //this method will update DataManager as ViewManager updates
-    exportPreferences(); //this method will export the current player preferences
-    exportCurrentState(); //this method will export the current game state
+    parseData(DataObject do); //this method will be called to parse through the data object especially if it has nested object to be serialized
+    
+    saveToFile(); //once the parseData is called and the DataObject is serialized, we an now save the information to a file.
     
     }
