1e8f562921ddc21bcabc1710084413e2aa959f36
DANA PARK <dp178@duke.edu>
2018-03-31 15:04:03 -0400

    added interfaces

---
+++ src/gamePlayerInterfaces/ExportData.java
@@ -0,0 +1,14 @@
+package gamePlayerInterfaces;
+
+/**
+ *  This interface allows information from DataManager to be exported to save preferences and save the current game state.
+ */
+
+public interface ExportData{
+
+	public void exportCurrentState(); //exports current game state
+
+	public void exportPreferences(); //exports saved preferences
+	
+}
+
