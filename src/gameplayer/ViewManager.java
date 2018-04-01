9f0e5d3f525893ed20a2ff0506db028182e8329e
DANA PARK <dp178@duke.edu>
2018-04-01 18:48:10 -0400

    additions to ViewManager

--- src/gameplayer/ViewManager.java
+++ src/gameplayer/ViewManager.java
@@ -1,13 +1,57 @@
 package gameplayer;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
 /**
  * 
  * @author Brandon Dalla Rosa
  *
  */
 public class ViewManager {
+	
+	public static final int FRAMES_PER_SECOND = 60;
+	public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+	public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+	private Stage stg;
+	private Scene myScene;
+	private Group root;
+	private Timeline animation;
 
+	
+	
 	public ViewManager() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void start(Stage stage) {
+		
+	}
+	
+	private Scene setUpGame() {
+		return null;
+		
+	}
+	
+	private void animationFrame() {
+		
+			KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY),
+					e -> step(SECOND_DELAY));
+			if(animation!=null) {
+				animation.stop();
+			}
+			animation = new Timeline();
+			animation.setCycleCount(Timeline.INDEFINITE);
+			animation.getKeyFrames().add(frame);
+			animation.play();
+		}
+	
+	public void step(double delay) {
+		
 	}
 
 }
