194791f41b4d3a1e9a5e25d6a0ab7ba6a93db10a
DANA PARK <dp178@duke.edu>
2018-04-01 20:39:18 -0400

    added drop downs

--- src/gameplayer/Menu.java
+++ src/gameplayer/Menu.java
@@ -1,9 +1,8 @@
 package gameplayer;
 
 import java.util.ResourceBundle;
-
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
@@ -15,15 +14,24 @@ import javafx.scene.layout.Pane;
 public class Menu {
 	private HBox pane;
 	private static final String DEFAULT_RESOURCE_PACKAGE = "gamePlayerResources/";
-	private ResourceBundle menuProperties = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + "menu_options");
+	
+	private ResourceBundle speedProperties = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + "speed");
+	private ResourceBundle statusProperties = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + "status");
+	private ResourceBundle saveLoadProperties = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + "save_load");
+	private ComboBox<String> speedBox;
+	private ComboBox<String> statusBox;
+	private ComboBox<String> saveLoadBox;
+	private PulldownFactory pullDownFactory = new PulldownFactory();
 
 	public Menu() {
 		pane = new HBox(20);
 		pane.setAlignment(Pos.CENTER);
-		//Remove this later, just for example
-		for(int i=0;i<5;i++) {
-				pane.getChildren().add(new Button("button "+i));
-		}
+	
+		pane.getChildren().add(pullDownFactory.SpeedBox(speedBox , speedProperties));
+		pane.getChildren().add(pullDownFactory.StatusBox(statusBox , statusProperties));
+		pane.getChildren().add(pullDownFactory.SaveLoadBox(saveLoadBox , saveLoadProperties));
+
+
 
 			
 	}
--- src/gameplayer/PulldownFactory.java
+++ src/gameplayer/PulldownFactory.java
@@ -1,4 +1,9 @@
 package gameplayer;
+
+import java.util.ResourceBundle;
+
+import javafx.scene.control.ComboBox;
+
 /**
  * 
  * @author Brandon Dalla Rosa
@@ -8,6 +13,34 @@ public class PulldownFactory {
 
 	public PulldownFactory() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	protected ComboBox<String> SpeedBox (ComboBox<String>speedBox , ResourceBundle bundle) {
+		speedBox = new ComboBox<String>();
+		speedBox.setValue(getResources(bundle, "InitialCommand"));
+		speedBox.getItems().addAll(getResources(bundle, "SpeedUpCommand"),
+				getResources(bundle, "SlowDownCommand"));
+		return speedBox;
+	}
+	
+	protected ComboBox<String> StatusBox (ComboBox<String>statusBox , ResourceBundle bundle) {
+		statusBox = new ComboBox<String>();
+		statusBox.setValue(getResources(bundle, "InitialCommand"));
+		statusBox.getItems().addAll(getResources(bundle, "PauseGameCommand"), getResources(bundle, "PlayGameCommand"),
+				getResources(bundle, "ReplayGameCommand"));
+		return statusBox;
+	}
+	
+	protected ComboBox<String> SaveLoadBox (ComboBox<String>saveLoadBox , ResourceBundle bundle) {
+		saveLoadBox = new ComboBox<String>();
+		saveLoadBox.setValue(getResources(bundle, "InitialCommand"));
+		saveLoadBox.getItems().addAll(getResources(bundle, "SaveCommand"), getResources(bundle, "LoadCommand"));
+		return saveLoadBox;
+	}
+	
+	
+	protected String getResources(ResourceBundle bundle, String string) {
+		return bundle.getString(string);
 	}
 
 }
