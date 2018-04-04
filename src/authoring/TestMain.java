574220526e6f40d38041565241177dd43bde7f1f
Liam <lp139@duke.edu>
2018-04-03 22:56:36 -0400

    Added ImageBuilder auxillary class

--- src/authoring/TestMain.java
+++ src/authoring/TestMain.java
@@ -1,18 +1,16 @@
 package authoring;
 
+import authoring.right_components.ImageBuilder;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class TestMain extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-//		BorderPane testBP = new BorderPane();
-//		Scene testScene = new Scene(testBP);
-//		AuthoringEnvironment ae = new AuthoringEnvironment();
-//		Scene testScene = ae.display();
 		SplashScreen ss = new SplashScreen(primaryStage);
 		Scene testScene = ss.display();
 		primaryStage.setScene(testScene);
