1c6e27fad4899e85f6df4cd3f3687f7a16452843
Jennifer <jrc81@duke.edu>
2018-04-03 23:47:07 -0400

    nav pane css

--- src/authoring/NavigationPane.java
+++ src/authoring/NavigationPane.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import observables.Listener;
@@ -13,12 +14,13 @@ import observables.Subject;
 import resources.keys.AuthRes;
 
 //Left Pane
-public class NavigationPane extends StackPane implements Subject {
+public class NavigationPane extends StackPane implements Subject, GUIComponent {
 
 	private ArrayList<String> menuTitles = new ArrayList<String>(Arrays.asList("Entity Creator", "Actions and Events", "Level Preferences", "Storyboard"));
 	private ArrayList<String> icons = new ArrayList<String>(Arrays.asList("entity", "event", "level", "story"));
 	
 	public NavigationPane() {
+		this.getStyleClass().add("pane-back");
 		initializeButtons();
 	}
 
@@ -43,13 +45,22 @@ public class NavigationPane extends StackPane implements Subject {
 		Subject np = this;
 		for(int i = 0; i < menuTitles.size(); i++) {
 			String s = menuTitles.get(i);
-			System.out.println(icons.get(i));
 			ImageView iv = new ImageView(new Image(AuthRes.getString(icons.get(i))));
+			//will be done by image editing class
+			iv.setFitHeight(20);
+			iv.setFitWidth(20);
 			Button b = new Button(s, iv);
+			b.getStyleClass().add("button-nav");
 			b.setOnAction(e -> np.notifyListeners(s));
 			navOptions.getChildren().add(b);
 		}
 		this.getChildren().add(navOptions);		
+	}
+
+	@Override
+	public Pane getView() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
