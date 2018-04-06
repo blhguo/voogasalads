df92f5b9913478284b2226c69e6c8706c0269f92
Ben Hubsch <bah37@duke.edu>
2018-04-06 14:08:14 -0400

    Minor cleanup.

--- src/game_engine/test/EntityTest.java
+++ src/game_engine/test/EntityTest.java
@@ -1,18 +1,12 @@
 package game_engine.test;
 
-//import org.junit.Test;
-
-import game_engine.Component;
-import game_engine.Entity;
-import game_engine.components.Physics;
-
 public class EntityTest {
 	
 //	@Test
 	public void testComponentAdded() {
-		Entity e = new Entity();
-		Component c = new Physics(0, 0, 0);
-		e.addComponent(c);
+//		Entity e = new Entity();
+//		Component c = new Physics(0, 0, 0);
+//		e.addComponent(c);
 //		assertEquals(c, e.getComponent(Physics.class));
 	}
 }
