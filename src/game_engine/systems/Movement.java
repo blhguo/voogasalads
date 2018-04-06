ae18d4e2188abf4b2426bcd0545d8f5c0aa54261
Kevin Deng <ktd9@duke.edu>
2018-04-05 22:29:28 -0400

    Created MovementInputSystem

--- src/game_engine/systems/Movement.java
+++ src/game_engine/systems/Movement.java
@@ -13,6 +13,8 @@ import game_engine.components.Position;
 
 public class Movement extends System {
 	private static final Class<? extends Component> POSITION = Position.class;
+	private static final Class<? extends Component> PHYSICS= Physics.class;
+
 
 	public Movement(Engine engine) {
 		super(engine);
@@ -24,14 +26,9 @@ public class Movement extends System {
 		for (Entity e : getEngine().getEntitiesContaining(args)) {
 			Physics physics = (Physics) e.getComponent(PHYSICS);
 			Position position = (Position) e.getComponent(POSITION);
-<<<<<<< HEAD
-			position.setX(calcPos(position.getX(), elapsedTime, physics.getXVel(), physics.getAccel()));
-			position.setY(calcPos(position.getY(), elapsedTime, physics.getYVel(), physics.getAccel()));
-=======
 			position.setX(position.getX() + physics.getXVel() * elapsedTime);
 			position.setY(position.getY() + physics.getYVel() * elapsedTime);
 			physics.setYVel(physics.getYVel() + physics.getAccel() * elapsedTime);
->>>>>>> ba411d246320aae23f2e20dc06a41f02a664823f
 		}
 	}
 }
---
+++ src/game_engine/systems/MovementInputSystem.java
@@ -0,0 +1,5 @@
+package game_engine.systems;
+
+public class MovementInputSystem {
+
+}
