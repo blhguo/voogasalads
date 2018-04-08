8444a7364bc4debab6b43b15d51f18b538f03258
Liam <lp139@duke.edu>
2018-04-08 16:53:39 -0400

    Updated Canvas to take in size parameter

--- src/authoring/Canvas.java
+++ src/authoring/Canvas.java
@@ -7,13 +7,17 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Canvas implements GUIComponent{
-
+	private int size;
+	public Canvas(int size){
+		this.size = size;
+	}
 	public Pane getView(){
 		VBox box = new VBox();
 		StackPane pane = new StackPane();
-		box.getChildren().add(pane);
-		Rectangle rect = new Rectangle(pane.getWidth(), pane.getHeight());
+		//box.getChildren().add(pane);
+		Rectangle rect = new Rectangle(size, size);
 		rect.setStyle("-fx-background-color: rgba(0,0,0.5)");
+		box.getChildren().add(rect);
 		box.setPadding(new Insets(20, 20, 20, 20));
 		return box;
 	}
