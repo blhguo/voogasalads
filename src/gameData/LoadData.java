86853879f935e56e4b151a73c8f840b4988aadda
Harry Wang <hyw2@duke.edu>
2018-04-09 22:23:50 -0400

    yols

---
+++ src/gameData/LoadData.java
@@ -0,0 +1,67 @@
+package gameData;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+
+import game_engine.Level;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+public class LoadData {
+	private File f;
+	private List<Level> levels;
+	private static final int FILE_EXTENSION = 4;
+	private XStream mySerializer;
+
+	
+	public LoadData(File load) {
+		this.f = load;
+		this.mySerializer = new XStream(new DomDriver());
+		try {
+			openFile(this.f);
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void openFile(File file) throws ParserConfigurationException{
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder;
+        String filePath = file.getAbsolutePath();
+        String fileType = filePath.substring(filePath.length()-FILE_EXTENSION);
+        //maybe a func to check that the file is an xml extension
+        try {
+        		dBuilder = dbFactory.newDocumentBuilder();
+			Document doc;
+			try {
+				doc = dBuilder.parse(file);
+				doc.getDocumentElement().normalize();
+				int nums = Integer.parseInt(doc.getElementsByTagName("num").item(0).getTextContent());
+				for (int i=0;i<nums;i++) {
+					Level temp = (Level) mySerializer.fromXML((Reader) doc.getElementsByTagName("<data"+Integer.toString(i)+">"));
+					levels.add(temp);
+				}
+			} catch (SAXException e) {
+				return;
+			}
+        } catch (IOException e) {
+        		return;
+        }
+	}
+	
+	public List<Level> loadLevels(){
+		return levels;
+	}
+}
---
+++ src/gameData/Saver.java
@@ -0,0 +1,43 @@
+package gameData;
+
+import java.io.FileOutputStream;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+public class Saver {
+	
+	public static void main (String[] args) {
+//		XStream mySerializer = new XStream(new DomDriver());
+//		String res = "";
+//		try{   
+//			Abs t = new Abs(3, 4.0);
+//            FileOutputStream fos=new FileOutputStream("testout.xml");    
+//            try {
+//	    		    res = mySerializer.toXML(t);
+//	    	        fos.write("<?xml version=\"1.0\"?>".getBytes("UTF-8"));
+//	    	        fos.write("<data>".getBytes("UTF-8"));
+//	    	        byte[] bytes = res.getBytes("UTF-8");
+//	    	        fos.write(bytes);
+//	    	        fos.write("</data>".getBytes("UTF-8"));
+//	    	    }catch(Exception e) {
+//	    	    		System.out.println(e);
+//	    	    } 
+//            fos.close();    
+//            System.out.println("success...");    
+//           }catch(Exception e){System.out.println(e);}    
+//		try {
+//		    		Abs t1 = (Abs)mySerializer.fromXML(res);
+//		    		System.out.println("Object has been deserialized ");
+//		            System.out.println("c = " + t1.getChestC());
+//		            System.out.println("d = " + t1.getChestD());
+//		            t1.update();
+//		            System.out.println("c = " + t1.getChestC());
+//		            System.out.println("d = " + t1.getChestD());
+//		            System.out.println(t1.get());
+//		    }catch(Exception e) {
+//		    		System.out.println(e);
+//		    }
+		
+	} 
+}
