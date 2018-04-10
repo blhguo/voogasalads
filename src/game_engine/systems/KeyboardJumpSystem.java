a8c88fc173bd677332cba2d1225e9b7bec4b270d
Ben Hubsch <bah37@duke.edu>
2018-04-09 22:11:56 -0400

    Removed Position from KeyboardJumpSystem.

--- src/game_engine/systems/KeyboardJumpSystem.java
+++ src/game_engine/systems/KeyboardJumpSystem.java
@@ -31,13 +31,11 @@ public class KeyboardJumpSystem extends GameSystem{
 		for (Entity entity : getEngine().getEntitiesContaining(args)) {
 			for(InputEvent input : getEngine().getInput()){
 				KeyEvent key = (KeyEvent) getEngine().getInput().peek();
-				PhysicsComponent physics = (PhysicsComponent) entity.getComponent(PHYSICS);
-				PositionComponent pos = (PositionComponent) entity.getComponent(POSITION);				
+				PhysicsComponent physics = (PhysicsComponent) entity.getComponent(PHYSICS);		
 				KeyboardJumpInputComponent jumpInput = (KeyboardJumpInputComponent) entity.getComponent(KEYBOARD_JUMP_INPUT);
 				JumpComponent jump = (JumpComponent) entity.getComponent(JUMP);
 				Vector direction = jumpInput.getDirection(key.getCode());
 				if (direction.getY() == 1 && jump.getOnGround() && jump.getJumpsAllowed() != 0){
-//					pos.setY(pos.getY() + physics.getMaxYVel() * direction.getY() * elapsedTime);
 					physics.setCurrYVel(physics.getMaxYVel());
 					jump.setJumpsAllowed(jump.getJumpsAllowed() - 1);
 					getEngine().getInput().remove(input);
