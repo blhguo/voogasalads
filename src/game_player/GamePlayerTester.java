ec3afd39f2a0573a4b37ecd45bf979adb7399a68
Dana Park <dana.park@duke.edu>
2018-04-12 12:11:12 -0400

    Update GamePlayerTester.java

--- src/game_player/GamePlayerTester.java
+++ src/game_player/GamePlayerTester.java
@@ -5,10 +5,21 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import javafx.scene.input.KeyCode;
-
+/**
+ * 
+ * @author Brandon Dalla Rosa
+ * Simple test class for Player functionality regarding the DataManager class
+ *
+ */
 public class GamePlayerTester {
 	DataManager dataManager = new DataManager();
 	@Test
+	
+/**
+ * 
+ * method tests that Key Preferences are correctly saved by Player once set 
+ *
+ */
 	public void testKeyPreferences() {
 		dataManager.setKey("Front", KeyCode.W);
 		String ret = dataManager.getInput(KeyCode.W);
