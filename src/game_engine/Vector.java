2233034a24dc8452bc84785d864a9cb74d92b903
Ben Hubsch <bah37@duke.edu>
2018-04-12 12:32:19 -0400

    Javadoc part of Engine.

--- src/game_engine/Vector.java
+++ src/game_engine/Vector.java
@@ -1,19 +1,42 @@
 package game_engine;
 
+/**
+ * 
+ * @author benhubsch
+ * 
+ *         The Class Vector is a convenient data structure for much of the backend, given that many
+ *         abstractions can be represented using a Vector.
+ */
 public class Vector {
-	
+
 	private double myX;
 	private double myY;
-	
+
+	/**
+	 * Instantiates a new Vector object.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 */
 	public Vector(double x, double y) {
 		myX = x;
 		myY = y;
 	}
-	
+
+	/**
+	 * Gets the double object.
+	 *
+	 * @return double
+	 */
 	public double getX() {
 		return myX;
 	}
-	
+
+	/**
+	 * Gets the double object.
+	 *
+	 * @return double
+	 */
 	public double getY() {
 		return myY;
 	}
