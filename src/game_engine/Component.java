7f1237e27f42874af7ed84e8d382ab3b0f615c6d
Andy Nguyen <aln20@duke.edu>
2018-04-17 22:26:44 -0400

    made component an abstract class

--- src/game_engine/Component.java
+++ src/game_engine/Component.java
@@ -1,11 +1,21 @@
 package game_engine;
 
-
 /**
  * The Interface Component is just a flag interface.
  * Example: http://www.w3processing.com/index.php?subMenuLoad=java/oop/InterfaceFlag.php
  */
-public interface Component {
-	String getValue();
-	void setValue(String value);
+public abstract class Component {
+	private String myValue;
+	
+	public Component(String arg){
+		
+	}
+	
+	public String getValue(){
+		return myValue;
+	}
+	
+	public void setValue(String value){
+		myValue = value;
+	}
 }
---
+++ src/game_engine/components/NumberOfJumpsComponent.java
@@ -0,0 +1,5 @@
+package game_engine.components;
+
+public class NumberOfJumpsComponent {
+
+}
