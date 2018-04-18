e946b3d5ed7ca278b3c1fdbad097ccfd7e79b8b9
Andy Nguyen <aln20@duke.edu>
2018-04-17 22:43:15 -0400

    edited some more components

--- src/game_engine/components/keyboard/KeyboardJumpInputComponent.java
+++ src/game_engine/components/keyboard/KeyboardJumpInputComponent.java
@@ -1,10 +1,6 @@
 package game_engine.components.keyboard;
 
-import java.util.List;
-
 import game_engine.Component;
-import game_engine.Vector;
-import javafx.scene.input.KeyCode;
 
 public class KeyboardJumpInputComponent extends Component{
 	public KeyboardJumpInputComponent(String arg){
