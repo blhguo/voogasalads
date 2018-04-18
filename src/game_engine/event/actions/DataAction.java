afea6f70c9446e2be1bb5fa657816af25dce8558
Andy Nguyen <aln20@duke.edu>
2018-04-17 23:44:13 -0400

    fixed DataAction

--- src/game_engine/event/actions/DataAction.java
+++ src/game_engine/event/actions/DataAction.java
@@ -1,8 +1,5 @@
 package game_engine.event.actions;
 
-import java.lang.reflect.Method;
-import java.util.List;
-
 import game_engine.Component;
 import game_engine.Entity;
 import game_engine.event.Action;
@@ -10,26 +7,17 @@ import game_engine.event.Action;
 public class DataAction implements Action{
 	private Entity myEntity;
 	private Class<? extends Component> myComponentClass;
-	private Class<?> methodArgType;
-	private Method myMethod;
 	private String myValue;
 
-	public DataAction(Entity entity, Class<? extends Component> componentClass, Method method, String value){
+	public DataAction(Entity entity, Class<? extends Component> componentClass, String value){
 		myEntity = entity;
 		myComponentClass = componentClass;
-		myMethod = method;
 		myValue = value;
-		methodArgType = method.getParameterTypes()[0];
 	}
 
 	@Override
 	public void execute() {
 		Component myComponent = myEntity.getComponent(myComponentClass);
-		
-		// TODO: arg parsing util
-		
-		// get arg, get right type, pass
-		
-		//myMethod.invoke(myComponent, );
+		myComponent.setValue(myValue);
 	}
 }
