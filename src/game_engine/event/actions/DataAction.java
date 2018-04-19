71353c2bb34d1dbfcd35e8e6e158ef700c608bfd
Kevin Deng <ktd9@duke.edu>
2018-04-18 23:06:08 -0400

    added stuff

--- src/game_engine/event/actions/DataAction.java
+++ src/game_engine/event/actions/DataAction.java
@@ -6,10 +6,10 @@ import game_engine.event.Action;
 
 public class DataAction implements Action {
 	private Entity myEntity;
-	private Class<? extends Component> myComponentClass;
+	private Class<? extends Component<?>> myComponentClass;
 	private String myValue;
 
-	public DataAction(Entity entity, Class<? extends Component> componentClass, String value) {
+	public DataAction(Entity entity, Class<? extends Component<?>> componentClass, String value) {
 		myEntity = entity;
 		myComponentClass = componentClass;
 		myValue = value;
@@ -17,7 +17,7 @@ public class DataAction implements Action {
 
 	@Override
 	public void execute() {
-		Component myComponent = myEntity.getComponent(myComponentClass);
+		Component<?> myComponent = myEntity.getComponent(myComponentClass);
 		myComponent.setValue(myValue);
 	}
 }
