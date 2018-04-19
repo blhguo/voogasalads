0ed622d0a62f9ee3479985f4117c48d44cc5967f
JEREMY CHEN <jc587@duke.edu>
2018-04-18 23:19:35 -0400

    modified tester

---
+++ src/game_engine/event/actions/LevelAction.java
@@ -0,0 +1,13 @@
+package game_engine.event.actions;
+
+import game_engine.event.Action;
+
+public class LevelAction implements Action {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
