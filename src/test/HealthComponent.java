2308f8926f77eca5076082d1c1f51cf824347a8f
Andy Nguyen <aln20@duke.edu>
2018-04-18 21:22:10 -0400

    added generics

---
+++ src/test/HealthComponent.java
@@ -0,0 +1,8 @@
+package test;
+
+public class HealthComponent extends Component<Double>{
+	public HealthComponent(double val) {
+		super(val);
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ src/test/NumJumpsComponent.java
@@ -0,0 +1,8 @@
+package test;
+
+public class NumJumpsComponent extends Component<Integer>{
+	public NumJumpsComponent(int val) {
+		super(val);
+		// TODO Auto-generated constructor stub
+	}
+}
