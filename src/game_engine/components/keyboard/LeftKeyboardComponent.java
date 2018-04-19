a59803726cda1de2023540da1311c2ce8d21a003
Ben Hubsch <bah37@duke.edu>
2018-04-18 23:28:18 -0400

    Refactored PlayerView to reflect latest Components.

---
+++ src/game_engine/components/keyboard/LeftKeyboardComponent.java
@@ -0,0 +1,10 @@
+package game_engine.components.keyboard;
+
+import game_engine.Component;
+
+public class LeftKeyboardComponent extends Component<String>{
+	public LeftKeyboardComponent(String arg) {
+		super(arg);
+	}
+
+}
