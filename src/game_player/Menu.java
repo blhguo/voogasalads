caca496eded1b0f5c7bf05eda5e98b2b54752b8c
Brandon Dalla Rosa <brandon.dalla.rosa@duke.edu>
2018-04-18 23:30:56 -0400

    General refactoring after merge

--- src/game_player/Menu.java
+++ src/game_player/Menu.java
@@ -1,25 +1,11 @@
 package game_player;
 
-import java.util.ArrayList;
 import authoring.GameChooserScreen;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import javafx.scene.effect.ColorAdjust;
-import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -32,15 +18,9 @@ public class Menu {
 	
 	private HBox pane;
 	private PulldownFactory pullDownFactory;
-	private VBox keyPrefMenu;
-	private Button keyPrefButton;
 	private Button gameSelectionButton;
-	private Stage keyPrefStage;
 	private Stage gameSelectionStage;
 	private DataManager dataManager;
-	private KeyCode currentKey;
-	private Button currentPrefButton;	
-	private String currentPrefString;
 	private SettingsMenu settings;
 	public Menu(DataManager data, PulldownFactory pdf) {
 		pane = new HBox(20);
@@ -48,11 +28,11 @@ public class Menu {
 		dataManager = data;
 		pullDownFactory = pdf;
 		makePullDownMenus();
-		makeKeyPrefMenu();
 		settings = new SettingsMenu();
 		makeGameSelectionMenu();
 		settings.makeSettingsMenu(pane);
 		settings.makeSettingsStage();
+		KeyPrefMenu kpm = new KeyPrefMenu(dataManager,pane);
 	}
 	/**
 	 * Method to add the menu into the VBox for the View Manager
@@ -86,31 +66,6 @@ public class Menu {
 		pane.getChildren().add(gameSelectionButton);
 		
 	}
-
-	private void initKeyPrefMenu() {
-		ArrayList<String> inputs = (ArrayList<String>) dataManager.getInputCommands();
-		for(String s : inputs) {
-			HBox toAdd = new HBox(10);
-			toAdd.setAlignment(Pos.CENTER);
-			Label label = new Label(s);
-			label.getStyleClass().add("text-keypref");
-
-			Button button = new Button("ENTER");
-			button.getStyleClass().add("button-keypref");
-			button.setOnAction(click->{setPref(button,s);});
-			toAdd.getChildren().add(label);
-			toAdd.getChildren().add(button);
-			keyPrefMenu.getChildren().add(toAdd);
-		}
-	}
-	
-	private void setPref(Button button,  String string) {
-		currentPrefButton = button;
-		currentPrefString = string;
-	}
-	private void showPrefMenu() {
-		keyPrefStage.show();
-	}
 	/**
 	 * getter method for the Brightness Slider on the Settings Stage
 	 * 
@@ -136,20 +91,6 @@ public class Menu {
 		GameChooserScreen gc = new GameChooserScreen(gameSelectionStage);
 		//gameSelectionStage.setScene(gc.display());
 		gameSelectionStage.show();
-	}
-	
-
-   /**
-	 * method to set keys to their preferences in the keyPrefStage
-	 * 
-	 */
-	public void checkForInput(KeyCode code) {
-		currentKey = code;
-		currentPrefButton.getStyleClass().add("button-keypref");
-
-		currentPrefButton.setText(""+currentKey);
-		dataManager.setKey(currentPrefString, code);
-	}
-	
+	}	
 
 }
