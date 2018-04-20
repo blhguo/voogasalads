14de60f00692d35fb03aef23ac3817df9ed2c0cd
Andy Nguyen <aln20@duke.edu>
2018-04-19 21:34:37 -0400

    created three data action classes

--- src/game_engine/event/actions/DataMultiplyAction.java
+++ src/game_engine/event/actions/DataMultiplyAction.java
@@ -4,21 +4,21 @@ import game_engine.Component;
 import game_engine.Entity;
 import game_engine.event.Action;
 
-public class DataChangeAction implements Action {
+public class DataMultiplyAction implements Action{
 	private Entity myEntity;
 	private Class<? extends Component<Number>> myComponentClass;
-	private Number myDelta;
+	private Number myScale;
 
-	public DataChangeAction(Entity entity, Class<? extends Component<Number>> componentClass, Number delta) {
+	public DataMultiplyAction(Entity entity, Class<? extends Component<Number>> componentClass, Number scale) {
 		myEntity = entity;
 		myComponentClass = componentClass;
-		myDelta = delta;
+		myScale = scale;
 	}
 	
 	@Override
 	public void execute() {
 		Component<Number> comp = myEntity.getComponent(myComponentClass);
-		comp.setValue(myDelta.doubleValue() + comp.getValue().doubleValue());
+		comp.setValue(myScale.doubleValue() * comp.getValue().doubleValue());
 	}
 
 }
