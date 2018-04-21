4220e3d0257a1c370b5192945220ce6ba41b5deb
Kevin Deng <ktd9@duke.edu>
2018-04-21 14:18:52 -0400

    added projectile classes

---
+++ src/game_engine/components/projectile/ProjectileCollidedComponent.java
@@ -0,0 +1,7 @@
+package game_engine.components.projectile;
+
+import game_engine.components.collision.CollidedComponent;
+
+public class ProjectileCollidedComponent extends CollidedComponent {
+
+}
---
+++ src/game_engine/components/projectile/ProjectileXPosComponent.java
@@ -0,0 +1,11 @@
+package game_engine.components.projectile;
+
+import game_engine.components.position.XPosComponent;
+
+public class ProjectileXPosComponent extends XPosComponent{
+
+	public ProjectileXPosComponent(String arg) {
+		super(arg);
+	}
+
+}
