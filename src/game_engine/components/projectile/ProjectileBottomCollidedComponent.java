5e4b8bc6be620561cb296791ae9aca1147db3b5c
Kevin Deng <ktd9@duke.edu>
2018-04-21 14:22:17 -0400

    Added projectile components

--- src/game_engine/components/projectile/ProjectileBottomCollidedComponent.java
+++ src/game_engine/components/projectile/ProjectileBottomCollidedComponent.java
@@ -2,6 +2,6 @@ package game_engine.components.projectile;
 
 import game_engine.components.collision.CollidedComponent;
 
-public class ProjectileCollidedComponent extends CollidedComponent {
+public class ProjectileBottomCollidedComponent extends CollidedComponent{
 
 }
