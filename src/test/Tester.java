6799502d1edb4e5c4c471002f6c995d8bb9dd43d
Ben Hubsch <bah37@duke.edu>
2018-04-23 22:56:43 -0400

    Messed around with submodules; now reverting.

--- src/test/Tester.java
+++ src/test/Tester.java
@@ -1,16 +1,44 @@
 package test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+
+import game_engine.Engine;
+import game_engine.GameSystem;
 
 public class Tester {
 	public static void main(String args[]) {
-		 Reflections reflections = new Reflections("voogasalad_callussalad.src.game_engine");
+		Reflections reflections = new Reflections("game_engine", new SubTypesScanner(true));
+		Set<Class<? extends GameSystem>> allClasses = reflections.getSubTypesOf(GameSystem.class);
 
-		 Set<Class<? extends Object>> allClasses = 
-		     reflections.getSubTypesOf(Object.class);
-		 System.out.println(allClasses);
+		List<GameSystem> systems = new ArrayList<>();
+
+		allClasses.stream().filter(clazz -> !Modifier.isAbstract(clazz.getModifiers())).forEach(clazz -> {
+			try {
+				Parameter[] params = clazz.getDeclaredConstructors()[0].getParameters();
+				Constructor<?> ctor;
+				GameSystem system;
+				if (params.length > 0) {
+					ctor = clazz.getDeclaredConstructor(new Class[] { Engine.class });
+					system = (GameSystem) ctor.newInstance(new Engine());
+				} else {
+					ctor = clazz.getDeclaredConstructor(new Class[] {});
+					system = (GameSystem) ctor.newInstance();
+				}
+				systems.add(system);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+
+		System.out.println(systems);
+
 	}
 }
-
