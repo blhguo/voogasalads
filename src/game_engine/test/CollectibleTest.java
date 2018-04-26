0e4123f966ef429a040fcb7ed39d77a8393eb137
Ben Hubsch <bah37@duke.edu>
2018-04-26 17:34:37 -0400

    #79 Integration all complete; Calculation still wrong with isInView.

--- src/game_engine/test/CollectibleTest.java
+++ src/game_engine/test/CollectibleTest.java
@@ -1,5 +1,6 @@
 package game_engine.test;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,16 +28,19 @@ import game_engine.components.physics.YAccelComponent;
 import game_engine.components.physics.YVelComponent;
 import game_engine.components.position.XPosComponent;
 import game_engine.components.position.YPosComponent;
+import game_engine.components.sprite.FilenameComponent;
+import game_engine.components.sprite.HeightComponent;
+import game_engine.components.sprite.WidthComponent;
 import game_engine.level.Level;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -45,12 +49,9 @@ public class CollectibleTest extends Application {
 	private Entity mainCharacter; //smol rect
 	private Entity coin; //BIG rect
 
-	Map<Entity, Rectangle> spritesMap = new HashMap<Entity, Rectangle>();
+	Map<Entity, ImageView> spritesMap = new HashMap<>();
 
 	private Engine engine;
-
-	private Rectangle mainCharacterRect;
-	private Rectangle coinRect;
 
 	private static final int FRAMES_PER_SECOND = 60;
 	private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
@@ -112,11 +113,11 @@ public class CollectibleTest extends Application {
 		root.getChildren().clear();
 		for (Entity entity : engine.getLevel().getEntities()) {
 			if (!spritesMap.isEmpty()){
-				Rectangle r = spritesMap.get(entity);
+				ImageView r = spritesMap.get(entity);
 				double xPos = entity.getComponent(XPosComponent.class).getValue();
 				double yPos = entity.getComponent(YPosComponent.class).getValue();
-				double width = entity.getComponent(HitboxWidthComponent.class).getValue();
-				double height = entity.getComponent(HitboxHeightComponent.class).getValue();
+				double width = entity.getComponent(WidthComponent.class).getValue();
+				double height = entity.getComponent(HeightComponent.class).getValue();
 				r.setX(xPos - width/2);
 				r.setY(yPos - height/2);
 				root.getChildren().add(r);
@@ -126,13 +127,14 @@ public class CollectibleTest extends Application {
 
 	private void buildMainCharacterEntity(){
 		mainCharacter = new Entity();
+		
 		//components needed for each of the 4 keyboard movement systems
 		mainCharacter.addComponent(new DefaultXVelComponent("200"));
 		mainCharacter.addComponent(new DefaultYVelComponent("200"));
-		mainCharacter.addComponent(new LeftKeyboardComponent(KeyCode.LEFT.toString()));
-		mainCharacter.addComponent(new RightKeyboardComponent(KeyCode.RIGHT.toString()));
-		mainCharacter.addComponent(new UpKeyboardComponent(KeyCode.UP.toString()));
-		mainCharacter.addComponent(new DownKeyboardComponent(KeyCode.DOWN.toString()));
+		mainCharacter.addComponent(new LeftKeyboardComponent(KeyCode.A.toString()));
+		mainCharacter.addComponent(new RightKeyboardComponent(KeyCode.D.toString()));
+		mainCharacter.addComponent(new UpKeyboardComponent(KeyCode.W.toString()));
+		mainCharacter.addComponent(new DownKeyboardComponent(KeyCode.S.toString()));
 		mainCharacter.addComponent(new XVelComponent("0"));
 		mainCharacter.addComponent(new YVelComponent("0"));
 		mainCharacter.addComponent(new PrimeComponent());
@@ -143,7 +145,7 @@ public class CollectibleTest extends Application {
 
 		//extra components needed for position system
 		mainCharacter.addComponent(new XPosComponent("500"));
-		mainCharacter.addComponent(new YPosComponent("350"));
+		mainCharacter.addComponent(new YPosComponent("300"));
 
 		//extra components needed for collision detection
 		mainCharacter.addComponent(new HitboxHeightComponent("100.0"));
@@ -159,6 +161,7 @@ public class CollectibleTest extends Application {
 
 	private void buildCoinEntity(){
 		coin = new Entity();
+		
 		//component needed for collectible system
 		coin.addComponent(new CollectibleComponent("50"));
 
@@ -168,7 +171,7 @@ public class CollectibleTest extends Application {
 		coin.addComponent(new HitboxWidthComponent("100.0"));
 		coin.addComponent(new HitboxXOffsetComponent("0.0"));
 		coin.addComponent(new HitboxYOffsetComponent("0.0"));
-		coin.addComponent(new XPosComponent("800"));
+		coin.addComponent(new XPosComponent("700"));
 		coin.addComponent(new YPosComponent("300"));
 	}
 
@@ -194,14 +197,34 @@ public class CollectibleTest extends Application {
 		double hh3 = coin.getComponent(HitboxHeightComponent.class).getValue();
 		double hw3 = coin.getComponent(HitboxWidthComponent.class).getValue();
 
-		mainCharacterRect = new Rectangle(x1 - hw1/2, y1 - hh1/2, hw1, hh1);
-		coinRect = new Rectangle(x3 - hw3/2, y3 - hh3/2, hw3, hh3);
+		// components needed for sprite component
+		mainCharacter.addComponent(new FilenameComponent("Mario.GIF"));
+		mainCharacter.addComponent(new HeightComponent("50"));
+		mainCharacter.addComponent(new WidthComponent("50"));
+		
+		// component needed for sprite component
+		coin.addComponent(new FilenameComponent("turtle.GIF"));
+		coin.addComponent(new HeightComponent("50"));
+		coin.addComponent(new WidthComponent("50"));
+		
+		ImageView mainView = new ImageView(mainCharacter.getComponent(FilenameComponent.class).getValue());
+		ImageView coinView = new ImageView(coin.getComponent(FilenameComponent.class).getValue());
+		
+		Entity referencePoint = new Entity();
+		referencePoint.addComponent(new FilenameComponent("clouds.png"));
+		referencePoint.addComponent(new XPosComponent("500"));
+		referencePoint.addComponent(new YPosComponent("300"));
+		referencePoint.addComponent(new HeightComponent("500"));
+		referencePoint.addComponent(new WidthComponent("500"));
+		engine.getLevel().addEntity(referencePoint);
+		ImageView referenceView = new ImageView(referencePoint.getComponent(FilenameComponent.class).getValue());
 
-		spritesMap.put(mainCharacter, mainCharacterRect);
-		spritesMap.put(coin, coinRect);
+		spritesMap.put(mainCharacter, mainView);
+		spritesMap.put(coin, coinView);
+		spritesMap.put(referencePoint, referenceView);
 
-		root.getChildren().add(mainCharacterRect);
-		root.getChildren().add(coinRect);
+		root.getChildren().add(mainView);
+		root.getChildren().add(coinView);
 	}
 
 
