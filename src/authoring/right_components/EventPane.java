1d271532e49e06e4f05a977619dc1277ced702cb
Liam <lp139@duke.edu>
2018-04-27 18:36:15 -0400

    #86 Event screen basic

--- src/authoring/right_components/EventPane.java
+++ src/authoring/right_components/EventPane.java
@@ -4,13 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.sun.prism.paint.Color;
+
 import authoring.controllers.EntityController;
 import frontend_utilities.ButtonFactory;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TitledPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import resources.keys.AuthRes;
 
 /**
@@ -23,12 +30,43 @@ public class EventPane extends BasePane {
 	
 	private EntityController myController;
 	
+	private Pane start;
+	private Pane newEvent;
+	private Pane addCondition;
+	private Pane addAction;
+	private Pane viewEvents;
+	private VBox box;
+	private VBox subBox;
+	
 	public EventPane(EntityController e){
 		myController = e;
 	}
 	public EventPane(){
+		box = new VBox();
+		box = buildBasicView(AuthRes.getString("EventTitle"));
+		box.setAlignment(Pos.TOP_CENTER);
+		subBox = new VBox();
+		box.getChildren().add(subBox);
+		initStart();
+//		initNewEvent();
+//		initAddCondition();
+//		initAddAction();
+//		initViewEvents();
 	}
 	
+	private void initStart() {
+		start = new Pane();
+		Button create = ButtonFactory.makeButton(e -> {
+			//box.getChildren().remove(subBox);
+			subBox.getChildren().clear();
+			//subBox.getChildren().add(newEvent);
+			//box.getChildren().add(subBox);
+		});
+		HBox createBox = ButtonFactory.makeHBox("Create New Entity", 
+				null, create);
+		subBox.getChildren().add(createBox);
+		
+	}
 	public void setController(EntityController e){
 		myController = e;
 	}
@@ -38,11 +76,9 @@ public class EventPane extends BasePane {
 	 */
 	@Override
 	public Pane getView() {
-		VBox box = buildBasicView(AuthRes.getString("EventTitle"));
-		getButtonArray().stream().map((button) -> box.getChildren().add(button)).collect(Collectors.toList());
-		box.getChildren().addAll(newSeparator(), RelationshipView());
 		return box;
 	}
+	
 	
 	/**
 	 * BasePane method that initializes all the buttons for this right pane
