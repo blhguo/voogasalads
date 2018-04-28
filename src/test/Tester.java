6c61dac1d3a085b5bf1dd6cac6053004588ab4fd
Ben Hubsch <bah37@duke.edu>
2018-04-28 13:01:37 -0400

    #86 Working version of getting classes with certain annotations.

--- src/test/Tester.java
+++ src/test/Tester.java
@@ -1,7 +1,18 @@
 package test;
 
+import java.util.Set;
+
+import org.reflections.Reflections;
+
+import game_engine.event.conditions.DataConditionable;
+
 public class Tester {
 	public static void main(String args[]) {
-
+		Reflections reflections = new Reflections("game_engine.components");
+		Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(DataConditionable.class);
+		
+		for (Class<?> clazz : annotated) {
+			System.out.println(clazz.getSimpleName().replace("Component", ""));
+		}
 	}
 }
