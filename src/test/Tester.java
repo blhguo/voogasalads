a31eda5e605e5e32998a34f948abff66c54cede1
Ben Hubsch <bah37@duke.edu>
2018-04-28 13:02:56 -0400

    #86 Pulled out magic values into constants.

--- src/test/Tester.java
+++ src/test/Tester.java
@@ -7,12 +7,16 @@ import org.reflections.Reflections;
 import game_engine.event.conditions.DataConditionable;
 
 public class Tester {
+	
+	private static final String COMPONENT_FACTORY = "game_engine.components";
+	private static final String SUFFIX = "Component";
+	
 	public static void main(String args[]) {
-		Reflections reflections = new Reflections("game_engine.components");
+		Reflections reflections = new Reflections(COMPONENT_FACTORY);
 		Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(DataConditionable.class);
 		
 		for (Class<?> clazz : annotated) {
-			System.out.println(clazz.getSimpleName().replace("Component", ""));
+			System.out.println(clazz.getSimpleName().replace(SUFFIX, ""));
 		}
 	}
 }
