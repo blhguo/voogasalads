10bc938babe9788fe60016d2439739f53ba9ab1c
JEREMY CHEN <jc587@duke.edu>
2018-04-28 18:38:06 -0400

    added copy tester

--- src/game_engine/DeepCopyTester.java
+++ src/game_engine/DeepCopyTester.java
@@ -19,6 +19,40 @@ public class DeepCopyTester {
 		fc = new FilenameComponent("original");
 		
 		e1 = new Entity();
+		e1.addComponent(hc);
+		e1.addComponent(pc);
+		e1.addComponent(fc);
+		
+		e2=e1.copy();
+	}
+	
+	public void run() {
+		System.out.println("INITIAL STATES\n");
+		System.out.println("ENTITY 1");
+		printComponents(e1);
+		System.out.println("ENTITY 2 (copy)");
+		printComponents(e2);
+		
+		hc.setValue(69.0);
+		fc.setValue("yoloswag");
+		
+		System.out.println("POST STATES\n");
+		System.out.println("ENTITY 1 (changed");
+		printComponents(e1);
+		System.out.println("ENTITY 2 (shouldn't be)");
+		printComponents(e2);
+	}
+	
+	public void printComponents(Entity e) {
+		for(Component<?> c: e.getComponents()) {
+			System.out.println(c.getClass() + " : " + c.getValue());
+		}
+		System.out.println("\n");
+	}
+	
+	public static void main(String[] args) {
+		DeepCopyTester d = new DeepCopyTester();
+		d.run();
 	}
 	
 }
