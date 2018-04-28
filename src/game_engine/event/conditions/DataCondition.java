9373da728a4624d8848c563aca55897d3054eabc
Ben Hubsch <bah37@duke.edu>
2018-04-28 12:57:39 -0400

    #86 Removed extraneous comments from Data Condition.

--- src/game_engine/event/conditions/DataCondition.java
+++ src/game_engine/event/conditions/DataCondition.java
@@ -14,31 +14,17 @@ public class DataCondition implements Condition {
 	private Class<? extends Component<?>> myComponentClass;
 	private String myComparison;
 	private String myExpected;
-	
-//	private Component myComponent;
-	
+
 	public DataCondition(Entity entity, Class<? extends Component<?>> componentClass, String comparison, String expected) {
 		myEntity = entity;
 		myComponentClass = componentClass;
 		myComparison = comparison;
 		myExpected = expected;
 	}
-//	
-//	public DataCondition(Component component, String comparison, String expected) {
-//		myComponent = component;
-//		myComparison = comparison;
-//		myExpected = expected;
-//	}
-	
+
 	@Override
 	public boolean evaluate() {
-		String expression;
-//		if(myzEntity == null && myComponent != null) {
-//			zexpression = myComponent.getValue() + myComparison + myExpected; 
-//		}z
-//		else {
-			expression = myEntity.getComponent(myComponentClass).getValue() + myComparison + myExpected; 
-//		}
+		String expression = myEntity.getComponent(myComponentClass).getValue() + myComparison + myExpected; 
 		BigDecimal result =  new Expression(expression).eval();
 		return result.equals(BigDecimal.ONE);
 	}
