dd9fe4835196d95a6fcd4f80e1ff209979290dc3
Brandon Guo <blg19@duke.edu>
2018-04-29 14:52:27 -0400

    added config file hierarchy changes, now more flexible and can handle changes to config

--- src/resources/keys/AuthRes.java
+++ src/resources/keys/AuthRes.java
@@ -7,6 +7,7 @@ public class AuthRes {
 	public static final ResourceBundle RESOURCEKEYS = ResourceBundle.getBundle("resources.keys/authoringResources");
 	public static final ResourceBundle VOOGLE = ResourceBundle.getBundle("resources.keys/English");
 	public static final ResourceBundle VOOGLEIMAGES = ResourceBundle.getBundle("resources.keys/Image");
+	public static final ResourceBundle CONFIGKEYS = ResourceBundle.getBundle("gameData/configMap");
 
 	/**
 	 * Returns the corresponding String of a given String key
@@ -16,6 +17,10 @@ public class AuthRes {
 	public static String getString(String key) {
         return RESOURCEKEYS.getString(key);
     }
+	
+	public static String getStringKeys(String key) {
+		return CONFIGKEYS.getString(key);
+	}
 	
 	/**
 	 * Returns the corresponding int of a given String key
