e10e611a90ee3ac38fc6966c37e3aad5bf5c11b2
JEREMY CHEN <jc587@duke.edu>
2018-05-02 12:28:29 -0400

    SMALL FIX: removed e.printStackTrace() from authoring & game_data && added null checking for CollisionConditions

--- src/game_engine/systems/collision/CollisionResponseSystem.java
+++ src/game_engine/systems/collision/CollisionResponseSystem.java
@@ -14,10 +14,10 @@ import game_engine.components.collision.edge_collided.TopCollidedComponent;
 import game_engine.level.Level;
 
 public abstract class CollisionResponseSystem implements GameSystem {
-	private static final Class<? extends Component<List<Entity>>> TOP = TopCollidedComponent.class;
-	private static final Class<? extends Component<List<Entity>>> BOTTOM = BottomCollidedComponent.class;
-	private static final Class<? extends Component<List<Entity>>> RIGHT = RightCollidedComponent.class;
-	private static final Class<? extends Component<List<Entity>>> LEFT = LeftCollidedComponent.class;
+	protected static final Class<? extends Component<List<Entity>>> TOP = TopCollidedComponent.class;
+	protected static final Class<? extends Component<List<Entity>>> BOTTOM = BottomCollidedComponent.class;
+	protected static final Class<? extends Component<List<Entity>>> RIGHT = RightCollidedComponent.class;
+	protected static final Class<? extends Component<List<Entity>>> LEFT = LeftCollidedComponent.class;
 	private static final List<Class<? extends Component<?>>> COLLIDED_ARGS = Arrays.asList(TOP, BOTTOM, RIGHT, LEFT);
 
 	protected List<Entity> getCollidedEntities(Level level) {
@@ -28,11 +28,11 @@ public abstract class CollisionResponseSystem implements GameSystem {
 		return level.getEntitiesContainingAny(entities, COLLIDED_ARGS);
 	}
 
-	protected List<Entity> getAllCollidedWith(Entity e) {
+	protected <T> List<Entity> getAllCollidedWith(Entity e) {
 		List<Entity> entitiesCollidedWith = new ArrayList<>();
 
 		COLLIDED_ARGS.stream().forEach(cc -> {
-			List<Entity> others = (List<Entity>) e.getComponent(cc).getValue();
+			List<Entity> others = (List<Entity>) e.getComponent((Class <? extends Component<T>>)cc).getValue();
 			if (others != null) {
 				entitiesCollidedWith.addAll(others);
 			}
