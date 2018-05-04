b290664773c31ee067b0b5323fe59f13738db12b
Ben Hubsch <bah37@duke.edu>
2018-05-03 22:28:25 -0400

    Positioned sign.

--- src/game_player/PlayerView.java
+++ src/game_player/PlayerView.java
@@ -156,8 +156,8 @@ public class PlayerView {
 //		healthData = primary.getComponent(HealthComponent.class).getValue();
 		double x = cam.getLayoutX();
 		double y = cam.getLayoutY();
-		viewManager.createText(x + SCORE_POS[0] - camInX, y + SCORE_POS[1] - camInY, "Score: " + scoreData);
-		viewManager.createText(x + HEALTH_POS[0] - camInX, y + HEALTH_POS[1] - camInY, "Health: " + healthData);
+//		viewManager.createText(x + SCORE_POS[0] - camInX, y + SCORE_POS[1] - camInY, "Score: " + scoreData);
+//		viewManager.createText(x + HEALTH_POS[0] - camInX, y + HEALTH_POS[1] - camInY, "Health: " + healthData);
 	}
 
 	/**
